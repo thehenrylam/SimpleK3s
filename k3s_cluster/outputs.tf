fad9f6c817fcce4a09fd695129baa28314ea9039
thehenrylam <the.henry.lam1@gmail.com>
2025-12-26 21:35:03 -0500

    Add description for the different outputs

--- k3s_cluster/outputs.tf
+++ k3s_cluster/outputs.tf
@@ -4,10 +4,12 @@ output "k3s_cluster_load_balancer" {
 }
 
 output "k3s_controller_public_ip" {
-  value = aws_instance.ec2_node[0].public_ip
+    description = "K3s controller's Public IP"
+    value       = aws_instance.ec2_node[0].public_ip
 }
 
 output "ssh_private_key_path" {
-  value = local_file.private_key.filename
+    description = "SSH Private Key Path"
+    value       = local_file.private_key.filename
 }
 
