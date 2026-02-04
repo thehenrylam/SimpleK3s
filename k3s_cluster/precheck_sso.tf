85b1282bec07017f6c6492628e6d675f8aac8fc3
thehenrylam <the.henry.lam1@gmail.com>
2026-02-03 21:00:10 -0500

    feature/#23 - Fix SSM parameter prechecks

--- k3s_cluster/precheck_sso.tf
+++ k3s_cluster/precheck_sso.tf
@@ -4,11 +4,11 @@ data "aws_ssm_parameter" "issuer" {
 }
 
 data "aws_ssm_parameter" "client" {
-    name            = var.idp_ssm_param_names.issuer
+    name            = var.idp_ssm_param_names.client
     with_decryption = true
 }
 
 data "aws_ssm_parameter" "secret" {
-    name            = var.idp_ssm_param_names.issuer
+    name            = var.idp_ssm_param_names.secret
     with_decryption = true
 }
